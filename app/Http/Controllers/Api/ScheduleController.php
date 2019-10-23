5f70755e141bb710f5a123985ccef157d7d5d781
xebraj <neff.esea@gmail.com>
2019-10-22 20:05:03 -0400

    Change schedule response format

--- app/Http/Controllers/Api/ScheduleController.php
+++ app/Http/Controllers/Api/ScheduleController.php
@@ -17,7 +17,7 @@ class ScheduleController extends Controller
             'date' => 'required|date_format:"Y-m-d"',
             'doctor_id' => 'required|exists:users,id'
         ];
-        $this->validate($request, $rules);
+        $request->validate($rules);
         // dd($request->all());
         
         $date = $request->input('date');
