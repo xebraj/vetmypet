9f23bf3c740a827530aaf50584b6ca3c33995296
xebraj <neff.esea@gmail.com>
2019-10-24 01:21:28 -0400

    Fix Appointment API

--- app/Http/Controllers/Api/AppointmentController.php
+++ app/Http/Controllers/Api/AppointmentController.php
@@ -40,7 +40,8 @@ class AppointmentController extends Controller
 
     public function store(StoreAppointment $request) 
     {
-        $patientId = Auth::guard('api')->id()
+        $patientId = Auth::guard('api')->id();
+        
         $appointment = Appointment::createForPatient($request, $patientId);
         if ($appointment)
             $success = true;
