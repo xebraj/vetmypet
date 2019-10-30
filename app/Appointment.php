dbe3adced378852bffac8050f628133331a5cc2e
xebraj <neff.esea@gmail.com>
2019-10-30 00:57:25 -0400

    Actualizacion de appointments

--- app/Appointment.php
+++ app/Appointment.php
@@ -3,77 +3,66 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class Appointment extends Model
 {
     protected $fillable = [
-        
-        'description',
-        'specialty_id',
-        'doctor_id',
-        'patient_id',
-        'scheduled_date',
-        'scheduled_time',
-        'type'
+    	'description', 
+    	'specialty_id',
+    	'doctor_id',
+    	'patient_id',
+    	'scheduled_date',
+    	'scheduled_time',
+    	'type'
     ];
-
     protected $hidden = [
         'specialty_id', 'doctor_id', 'scheduled_time'
     ];
-
     protected $appends = [
         'scheduled_time_12'
     ];
-
-    
     // N $appointment->specialty 1
-    public function specialty(){
-        return $this->belongsTo(Specialty::class);
+    public function specialty()
+    {
+    	return $this->belongsTo(Specialty::class);
     }
-
     // N $appointment->doctor 1
-    public function doctor(){
+    public function doctor()
+    {
         return $this->belongsTo(User::class);
     }
-
-    // n $appointment->patient 1
-    public function patient(){
+    // N $appointment->patient 1
+    public function patient()
+    {
         return $this->belongsTo(User::class);
     }
-
-    // 1 Appointment  hasOne -  1/0 CancelledAppointment
+    // Appointment hasOne 1 - 1/0 belongsTo CancelledAppointment
     // $appointment->cancellation->justification
     public function cancellation()
     {
         return $this->hasOne(CancelledAppointment::class);
     }
-
     // accessor
     // $appointment->scheduled_time_12
-    public function getScheduledTime12Attribute(){
+    public function getScheduledTime12Attribute() {
         return (new Carbon($this->scheduled_time))
-        ->format('g:i A');
+            ->format('g:i A');
     }
-
     static public function createForPatient(Request $request, $patientId) {
         $data = $request->only([
-            'description',
+            'description', 
             'specialty_id',
             'doctor_id',
             'scheduled_date',
             'scheduled_time',
             'type'
         ]);
-
         $data['patient_id'] = $patientId;
-
-        // Cambia formato de fecha
-        $carbonTime = Carbon::CreateFromFormat('g:i A', $data['scheduled_time']);
+        // right time format
+        $carbonTime = Carbon::createFromFormat('g:i A', $data['scheduled_time']);
         $data['scheduled_time'] = $carbonTime->format('H:i:s');
-
         return self::create($data);
-    }
+    } 
 }
--- app/Http/Controllers/Api/AppointmentController.php
+++ app/Http/Controllers/Api/AppointmentController.php
@@ -11,43 +11,44 @@ use App\Http\Requests\StoreAppointment;
 
 class AppointmentController extends Controller
 {
-    public function index() 
+    public function index()
     {
-        $user = Auth::guard('api')->user();
-        $appointments = $user->asPatientAppointments()
-        ->with([
-            'specialty' => function ($query) {
-                $query->select('id', 'name');
-            },
-            'doctor' => function ($query) {
-                $query->select('id', 'name');
-            }
-        ])
-        ->get([
-            "id",
-            "description",
-            "specialty_id",
-            "doctor_id",
-            "scheduled_date",
-            "scheduled_time",
-            "type",
-            "created_at",
-            "status"
-        ]);
-
-        return $appointments;
+    	$user = Auth::guard('api')->user();
+    	$appointments = $user->asPatientAppointments()
+	    	->with([
+	    		'specialty' => function ($query) {
+	    			$query->select('id', 'name');
+	    		}, 
+	    		'doctor' => function ($query) {
+	    			$query->select('id', 'name');
+	    		}
+	    	])
+	    	->get([
+				"id",
+				"description",
+				"specialty_id",
+				"doctor_id",
+				"scheduled_date",
+				"scheduled_time",
+				"type",
+				"created_at",
+				"status"
+            ]);
+            
+    	return $appointments;
     }
 
-    public function store(StoreAppointment $request) 
+    public function store(StoreAppointment $request)
     {
-        $patientId = Auth::guard('api')->id();
-        
-        $appointment = Appointment::createForPatient($request, $patientId);
-        if ($appointment)
-            $success = true;
-        else
+    	$patientId = Auth::guard('api')->id();
+    	
+    	$appointment = Appointment::createForPatient($request, $patientId);
+    	
+    	if ($appointment) 
+    		$success = true;
+    	else 
             $success = false;
-
-        return compact('success');
+            
+    	return compact('success');
     }
 }
