f87977ee822fe88d5f08d401ea3f085a0f510652
xebraj <neff.esea@gmail.com>
2019-11-01 23:59:06 -0400

    Add use Auth in FirebaseController

--- app/Http/Controllers/Api/FirebaseController.php
+++ app/Http/Controllers/Api/FirebaseController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use Auth;
+
 class FirebaseController extends Controller
 {
     public function postToken(Request $request)
     {
         $user = Auth::guard('api')->user();
-        
+
         if ($request->has('device_token')) {
         $user->device_token = $request->input('device_token');
         $user->save();
